7930b78e6c6fcf886f85cf70d6fd638035e720ec
Jayshree Chauhan <jayshree.chauhan7397@gmail.com>
2025-05-09 17:58:44 +0530

    added more files

---
+++ ch2_variables_and_data_types/3_type_function_and_typecasting.py
@@ -0,0 +1,36 @@
+
+a=31
+t = type(a)   
+print(t)
+
+a=31.76
+t = type(a)   
+print(t)
+
+a="hellow"
+t = type(a)   
+print(t)
+
+
+
+#---------------typecasting
+
+print("-------Typecasting-------")
+
+a = "123.44"
+t = type(a)
+print(t)
+
+t = float(a)
+print(t)
+
+t = int(float(a))
+print(t)
+
+
+a=123.44
+t = type(a)
+print(t)
+
+t = str(a)
+print(t)
---
+++ ch2_variables_and_data_types/4_input_function.py
@@ -0,0 +1,8 @@
+
+a = input("Enter number : ")
+b = input("Enter number : ")
+
+print("a : ",a)
+print("b : ",b)
+print("a+b (concatination) : ",a+b)
+print("a+b (sum): ",int(a)+int(b))
